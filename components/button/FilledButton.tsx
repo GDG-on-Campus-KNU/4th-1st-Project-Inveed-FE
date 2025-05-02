60e5296397bfc5049a2ea88dcb0aabb07330e065
chae-won-shin <scwcart@naver.com>
2025-05-03 00:03:18 +0900

    style: FilledButton 컴포넌트 구현

---
+++ components/button/FilledButton.tsx
@@ -0,0 +1,17 @@
+import { View, Pressable, Text } from "react-native";
+import "@/global.css";
+import tw from "@/utils/tw";
+import GoogleLogo from "@/assets/images/google-logo.svg";
+
+const Filledbutton = () => {
+  return (
+    <View
+      style={tw`flex flex-row justify-center items-center w-full py-4 rounded-lg bg-white border border-gray-300`}
+    >
+      <GoogleLogo width={16} height={16} />
+      <Text style={tw`ml-2 text-sm text-left`}>Google로 시작하기</Text>
+    </View>
+  );
+};
+
+export default Filledbutton;
