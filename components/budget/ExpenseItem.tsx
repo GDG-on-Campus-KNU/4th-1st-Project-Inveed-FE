465f5576c0c94d3f90e6820bf42f2feacac599c3
chae-won-shin <scwcart@naver.com>
2025-05-03 03:18:27 +0900

    style: 지출내역 컴포넌트 구현

---
+++ components/budget/ExpenseItem.tsx
@@ -0,0 +1,48 @@
+import { View, Text } from "react-native";
+import "@/global.css";
+import tw from "@/utils/tw";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { getCategoryIcon } from "./iconMapper";
+
+type ExpenseTypeProps = {
+  category:
+    | "food"
+    | "cafe"
+    | "entertainment"
+    | "life"
+    | "beauty"
+    | "fashion"
+    | "health"
+    | "others";
+  name: string;
+  amount: number;
+};
+
+const ExpenseItem = ({ category, name, amount }: ExpenseTypeProps) => {
+  const { name: iconName, color, title } = getCategoryIcon(category);
+  const isNegative = amount < 0;
+
+  return (
+    <View
+      style={tw`flex flex-row w-full justify-center items-center px-4 py-3 `}
+    >
+      <View
+        style={tw`mr-3 items-center justify-center w-8 h-8 bg-white rounded-full`}
+      >
+        <FontAwesome5 name={iconName} size={16} color={color} />
+      </View>
+
+      <View style={tw`flex flex-col justify-center items-start flex-grow`}>
+        <Text style={tw`text-black`}>{name}</Text>
+        <Text style={tw`text-xs text-gray-400 mt-0.5`}>{title}</Text>
+      </View>
+      <Text style={tw`${isNegative ? "text-gray-400" : "text-black"}`}>
+        {isNegative
+          ? `- ${Math.abs(amount).toLocaleString()}원`
+          : `+ ${amount.toLocaleString()}원`}
+      </Text>
+    </View>
+  );
+};
+
+export default ExpenseItem;
