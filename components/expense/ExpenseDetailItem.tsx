6d3f0568997b2ec09c990a5e8bdc9f6c6024202b
chominju <cmj021002@knu.ac.kr>
2025-05-04 00:25:00 +0900

    feat: ExpenseDetailItem 컴포넌트 구현

---
+++ components/expense/ExpenseDetailItem.tsx
@@ -0,0 +1,45 @@
+import { View, Text, Image } from 'react-native';
+import '@/global.css';
+
+type ExpenseDetailItemProps = {
+  date: string;
+  img_url: string;
+  name: string;
+  detail: string;
+  price: number;
+  type: 'income' | 'expense';
+};
+
+export const ExpenseDetailItem = ({
+  date,
+  img_url,
+  name,
+  detail,
+  price,
+  type = 'expense',
+}: ExpenseDetailItemProps) => {
+  return (
+    <View className="flex flex-col w-full rounded-xl p-3 bg-white gap-2 shadow-md shadow-gray-300">
+      <Text className="text-title text-sm">{date}</Text>
+      <View className="flex flex-row w-full justify-between">
+        <View className="flex flex-row">
+          <Image
+            source={{ uri: img_url }}
+            className="w-12 h-12 rounded-full mr-md"
+          />
+          <View className="flex flex-col gap-0.5">
+            <Text>{name}</Text>
+            <Text className="text-xs text-label">{detail}</Text>
+          </View>
+        </View>
+        <Text
+          className={
+            type === 'income' ? 'text-blue font-bold' : 'font-bold text-red'
+          }
+        >
+          {price}원
+        </Text>
+      </View>
+    </View>
+  );
+};
