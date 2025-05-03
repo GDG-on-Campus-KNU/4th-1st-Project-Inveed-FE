b6093a36928ea227e1ad3689ae7327dc2d75829f
chominju <cmj021002@knu.ac.kr>
2025-05-04 00:24:16 +0900

    feat: add gray option

--- components/button/ColoredButton.tsx
+++ components/button/ColoredButton.tsx
@@ -1,35 +1,37 @@
-import { Pressable, Text } from "react-native";
-import "@/global.css";
-import tw from "@/utils/tw";
+import { Pressable, Text } from 'react-native';
+import '@/global.css';
+import tw from '@/utils/tw';
 
 type ColoredButtonProps = {
-  color?: "green" | "blue";
+  color?: 'green' | 'blue' | 'gray';
   text: string;
-  size?: "sm" | "md" | "lg";
+  size?: 'sm' | 'md' | 'lg';
 };
 
 const ColoredButton = ({
-  color = "green",
+  color = 'green',
   text,
-  size = "lg",
+  size = 'lg',
 }: ColoredButtonProps) => {
-  const bgColor = color === "green" ? "bg-green-500" : "bg-blue-500";
-  const borderColor =
-    color === "green" ? "border-green-500" : "border-blue-500";
-  const textColor = color === "green" ? "text-white" : "text-white";
+  const bgColor =
+    color === 'green'
+      ? 'bg-green-500'
+      : color === 'blue'
+        ? 'bg-blue-500'
+        : 'bg-gray-400';
 
   const width =
-    size === "lg"
-      ? "w-full"
-      : size === "md"
-      ? "w-1/2"
-      : "w-auto self-start py-1";
+    size === 'lg'
+      ? 'w-full'
+      : size === 'md'
+        ? 'w-1/2'
+        : 'w-auto self-start py-1';
 
   return (
     <Pressable
-      style={tw`flex flex-row justify-center items-center py-4 ${width} rounded-lg ${bgColor} border border ${borderColor}`}
+      style={tw`flex flex-row justify-center items-center py-4 ${width} rounded-lg ${bgColor} border border-none`}
     >
-      <Text style={tw`ml-3 mr-3 text-sm text-left ${textColor}`}>{text}</Text>
+      <Text style={tw`ml-3 mr-3 text-sm text-left text-white`}>{text}</Text>
     </Pressable>
   );
 };
