bcfa0e43da03cfba6e1c5523f5a0700d4755d325
chae-won-shin <scwcart@naver.com>
2025-05-05 21:38:24 +0900

    fix: transactionInput 색상 수정

--- components/budget/Input/TransactionInput.tsx
+++ components/budget/Input/TransactionInput.tsx
@@ -1,23 +1,18 @@
 import { View, Text, TextInput } from "react-native";
-import "@/global.css";
-import tw from "@/utils/tw";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Colors } from "@/constants/Colors";
+import { colors } from "@/constants";
 
 const TransactionInput = () => {
   return (
-    <View
-      style={tw`flex flex-row justify-between items-center w-full px-4 py-5 border-b border-gray-300`}
-    >
-      <Text style={tw`w-1/5 text-start font-semibold text-gray-500`}>
+    <View className="flex flex-row justify-between items-center w-full px-4 py-5 border-b border-label">
+      <Text className="w-1/5 text-start font-semibold text-title">
         거래내용
       </Text>
       <TextInput
         placeholder="거래내용을 입력해 주세요"
-        placeholderTextColor="#9CA3AF"
-        style={[
-          tw`flex-grow text-start text-black border-none outline-none`,
-          { outlineStyle: "none", borderWidth: 0 },
-        ]}
+        placeholderTextColor={colors.LABEL}
+        className="flex-grow text-start text-black border-none outline-none"
         underlineColorAndroid="transparent"
         selectionColor="#00000000"
       />
