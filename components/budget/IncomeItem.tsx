25d7c6f3397a7ca3c6bb34978343c9ca1a56b2a1
chae-won-shin <scwcart@naver.com>
2025-05-05 21:44:28 +0900

    fix: expense/incomeItem 색상 수정

--- components/budget/IncomeItem.tsx
+++ components/budget/IncomeItem.tsx
@@ -1,6 +1,4 @@
 import { View, Text } from "react-native";
-import "@/global.css";
-import tw from "@/utils/tw";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { getCategoryIcon } from "./iconMapper";
 
@@ -14,20 +12,16 @@ const InputItem = ({ category, name, amount }: ExpenseTypeProps) => {
   const { name: iconName, color, title } = getCategoryIcon(category);
 
   return (
-    <View
-      style={tw`flex flex-row w-full justify-center items-center px-4 py-3 `}
-    >
-      <View
-        style={tw`mr-3 items-center justify-center w-8 h-8 bg-white rounded-full`}
-      >
+    <View className="flex flex-row w-full justify-center items-center px-4 py-3">
+      <View className="mr-3 items-center justify-center w-8 h-8 bg-white rounded-full">
         <FontAwesome5 name={iconName} size={16} color={color} />
       </View>
 
-      <View style={tw`flex flex-col justify-center items-start flex-grow`}>
-        <Text style={tw`text-black`}>{name}</Text>
-        <Text style={tw`text-xs text-gray-400 mt-0.5`}>{title}</Text>
+      <View className="flex flex-col justify-center items-start flex-grow">
+        <Text className="text-black">{name}</Text>
+        <Text className="text-xs text-title mt-0.5">{title}</Text>
       </View>
-      <Text style={tw`text-black`}>+ {amount.toLocaleString()}원</Text>
+      <Text className="text-black">+ {amount.toLocaleString()}원</Text>
     </View>
   );
 };
