8bbc7f74574a390eedd9ea79d9a3c4a8ad8d8323
chae-won-shin <scwcart@naver.com>
2025-05-05 21:24:34 +0900

    fix: totalExpense 색상 수정

--- components/Home/TotalExpense.tsx
+++ components/Home/TotalExpense.tsx
@@ -1,5 +1,4 @@
 import { View, Text } from "react-native";
-import "@/global.css";
 import tw from "@/utils/tw";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
@@ -7,14 +6,10 @@ const TotalExpense = ({ amount }: { amount: number }) => {
   const formattedAmount = new Intl.NumberFormat("ko-KR").format(amount);
 
   return (
-    <View
-      style={tw`flex flex-row justify-start items-center w-full px-4 py-4 rounded-lg shadow-lg bg-white`}
-    >
-      <View style={tw`flex flex-col justify-start items-start flex-grow`}>
-        <Text style={tw`text-sm font-semibold text-gray-600`}>
-          이달의 총 지출
-        </Text>
-        <Text style={tw`text-xl font-bold mt-0.5 text-green-500`}>
+    <View className="flex flex-row justify-start items-center w-full px-4 py-4 rounded-lg shadow-lg bg-white">
+      <View className="flex flex-col justify-start items-start flex-grow">
+        <Text className="text-sm font-semibold text-title">이달의 총 지출</Text>
+        <Text className="text-xl font-bold mt-0.5 text-pointgreen">
           {formattedAmount}원
         </Text>
       </View>
