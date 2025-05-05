4e3d69fa6e11703a6f985fc01cb569397629f7c0
chae-won-shin <scwcart@naver.com>
2025-05-06 01:29:14 +0900

    feat: 로그인 페이지 이동 구현

--- pages/auth/LoginPage.tsx
+++ pages/auth/LoginPage.tsx
@@ -1,21 +1,29 @@
 import { View, Text, StyleSheet } from "react-native";
+
 import InveedLogo from "@/assets/images/InveedLogo.svg";
 import TextLogo from "@/assets/images/TextLogo.svg";
+import GoogleLogo from "@/assets/images/google-logo.svg";
+
 import Loginbutton from "@/components/button/LoginButton";
+
 import { useRouter } from "expo-router";
 
 const LoginPage = () => {
   const router = useRouter();
 
-  const handleLogin = async () => {
-    router.replace("/");
+  const handleLogin = () => {
+    router.push("/token");
   };
 
   return (
     <View style={styles.container}>
       <InveedLogo />
       <TextLogo />
-      <Loginbutton onPress={handleLogin} />
+      <Loginbutton
+        content="Google로 시작하기"
+        icon={GoogleLogo}
+        onPress={handleLogin}
+      />
       <View>
         <Text className="text-base text-center text-label">
           로그인하여 Inveed와 함께
