264620f9bf229c21aff9c083402d8b41d9603a9e
chae-won-shin <scwcart@naver.com>
2025-05-06 00:56:36 +0900

    style: 로그인 페이지 구현

--- components/button/LoginButton.tsx
+++ components/button/LoginButton.tsx
@@ -1,9 +1,12 @@
 import { Pressable, Text } from "react-native";
 import GoogleLogo from "@/assets/images/google-logo.svg";
 
-const Loginbutton = () => {
+const Loginbutton = ({ onPress }: { onPress: () => void }) => {
   return (
-    <Pressable className="flex flex-row justify-center items-center w-full py-4 rounded-lg bg-white border border-gray-200">
+    <Pressable
+      onPress={onPress}
+      className="w-4/5 flex flex-row justify-center items-center w-full py-4 rounded-lg bg-white border border-gray-200"
+    >
       <GoogleLogo width={16} height={16} />
       <Text className="ml-2 text-sm text-left">Google로 시작하기</Text>
     </Pressable>
---
+++ pages/auth/LoginPage.tsx
@@ -0,0 +1,41 @@
+import { View, Text, StyleSheet } from "react-native";
+import InveedLogo from "@/assets/images/InveedLogo.svg";
+import TextLogo from "@/assets/images/TextLogo.svg";
+import Loginbutton from "@/components/button/LoginButton";
+import { useRouter } from "expo-router";
+
+const LoginPage = () => {
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    router.replace("/");
+  };
+
+  return (
+    <View style={styles.container}>
+      <InveedLogo />
+      <TextLogo />
+      <Loginbutton onPress={handleLogin} />
+      <View>
+        <Text className="text-base text-center text-label">
+          로그인하여 Inveed와 함께
+        </Text>
+        <Text className="text-base text-center text-label">
+          스마트한 주식 투자를 시작하세요!
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default LoginPage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 32,
+    gap: 32,
+  },
+});
